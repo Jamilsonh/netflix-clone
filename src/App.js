2a3e14c8afa1a0e54e993cbd1589c077bb727a4a
Jamilsonh <2jamilson@gmail.com>
2021-10-20 19:15:43 -0300

    Consturindo Front-end

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
+
+  const [movieList, setMovieList] = useState([]);
 
   useEffect(()=>{
     const loadAll = async () => {
       // Pegando a lista TOTAL
       let list = await Tmdb.getHomeList();
-      console.log(list);
+      setMovieList(list);
     }
     
     loadAll();
@@ -17,8 +20,14 @@ export default () => {
 
 
   return (
-    <div>
-      Olá Mundo!
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key)=>(
+          <div>
+            <MovieRow key={key} title={item.title} items={item.items}/>
+          </div>
+        ))}
+      </section>
     </div>
   );
 }
