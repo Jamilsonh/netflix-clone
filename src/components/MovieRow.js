bfbeceb4ac8b2f5e0a31bd15026aa92d5cb70e56
Jamilsonh <2jamilson@gmail.com>
2021-10-22 17:53:22 -0300

    Ajustando imagens

--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -6,6 +6,12 @@ export default ({title, items}) => {
     return (
         <div>
             <h2>{title}</h2>
+            <div className="movieRow--listarea">
+                {items.results.length > 0 && items.results.map((item, key)=>(
+                    // eslint-disable-next-line jsx-a11y/alt-text
+                    <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
+                ))}
+            </div>
         </div>
     );
 }
