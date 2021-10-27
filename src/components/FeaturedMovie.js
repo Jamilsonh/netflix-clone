8e6e5fedd3e83d55eb1abc64a2f906fd426b4ed0
Jamilsonh <2jamilson@gmail.com>
2021-10-26 22:55:24 -0300

    Atualização do projeto

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -2,10 +2,24 @@ import React from 'react';
 import './FeaturedMovie.css';
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default () => {
+export default ({item}) => {
     return (
-        <div>
-            FILME EM DESTAQUE
-        </div>
+        <section className="featured" style={{
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+        }}>
+            <div className="featured--vertical">
+                <div className="featured--horizontal">
+                    <div className="featured--name">{item.original_name}</div>
+                    <div className="featured--info">
+                        <div className="featured--points">{item.vote_average} pontos</div>
+                        <div className="featured--year">2099</div>
+                        <div className="featured--seasons">{item.number_of_seasons} temporadas </div>
+                    </div>
+                </div>
+            </div>
+        
+        </section>
     );
 }
